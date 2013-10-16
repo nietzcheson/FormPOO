dab9b2e159939e490ac8d70c2dc46e97d6112e89
cristianangulonova <cristianangulonova@MacBook-Pro-de-Cristian.local>
2013-10-16 10:17:16 -0500

    Versión lista de la clase formPOO
    Facilidad en la creación de formularios webs con php. La clase se
    encarga de graficar formularios y cada uno de sus elementos...

--- formPOO.php
+++ formPOO.php
@@ -25,19 +25,44 @@ class Formulario
 	{?>
 
 <?php foreach($this->forms as $this->form):?>
-<form method="<?=$this->form['method']?>" action="<?=$this->form['action']?>" id="<?=$this->form['id']?>">
+<form method="<?=$this->form['method']?>" action="<?=$this->form['action']?>" id="<?=$this->form['id']?>" class="<?=$this->form['class']?>">
+<legend><?=$this->form["legend"]?></legend>
 <?php foreach($this->inputs as $this->input):?>
+
 <?php if($this->input['type']=='select'):?>
-<select name="<?=$this->input['name']?>">
+	<div class="<?=$this->input['class']?>">
+		<label><?=$this->input['label']?></label>
+	<select name="<?=$this->input['name']?>">
 <?php foreach($this->input["opciones"] as $this->opciones):?>
 <option><?=$this->opciones?></option>
 <?php endforeach;?>
 </select>
+	</div>
+	
 <?php elseif($this->input['type']=='checkbox' || $this->input['type']=='radio'):?>
+	<label><?=$this->input['label']?></label>
 	<input type="<?=$this->input['type']?>" name="<?=$this->input['name']?>" value="<?=$this->input['value']?>"/>
 
+<?php elseif($this->input['type']=='submit'):?>
+	<div class="<?=$this->input['class']?>">
+	<input type="<?=$this->input['type']?>" value="<?=$this->input['value']?>"/>
+	</div>
+<?php elseif($this->input['type']=="date"):?>
+	<div class="<?=$this->input['class']?>">
+		<label><?=$this->input['label']?></label>
+	<input type="text" name="<?=$this->input['name']?>" placeholder="Mes/Día/Año" class="datepicker"/>
+</div>
+<?php elseif($this->input['type']=="textarea"): ?>
+	<div class="<?=$this->input['class']?>">
+		<label><?=$this->input['label']?></label>
+	<textarea name="<?=$this->input['name']?>"></textarea>
+</div>
 <?php else:?>
-<input type="<?=$this->input['type']?>" name="<?=$this->input['name']?>" value="<?=$this->input['value']?>" placeholder="<?=$this->input['placeholder']?>" class="<?=$this->input['class']?>"/>
+	<div class="<?=$this->input['class']?>">
+		<label><?=$this->input['label']?></label>
+<input type="<?=$this->input['type']?>" name="<?=$this->input['name']?>" value="<?=$this->input['value']?>" placeholder="<?=$this->input['placeholder']?>"/>
+	</div>
+	
 <?php endif;?>
 <?php endforeach;?>
 </form>
@@ -49,16 +74,21 @@ class Formulario
 		
 	}
 }
-//Datos principales del formulario
+
+
+/*
 $formularios["registro"] = [
 	"method"=>"POST",
 	"action"=>"form.php",
-	"id"=>"formulario"
+	"id"=>"formulario",
+	"legend"=>"Datos del formulario"
+
 ];
 
 	//Input Text
 
 	$inputs["nombre"] = [
+		"label"=>"Label del input",
 		"type"=>"text",
 		"name"=>"Nombre",
 		"value"=>"",
@@ -68,6 +98,7 @@ $formularios["registro"] = [
 
 	//Select
 	$inputs["paises"] = [
+		"label"=>"Label del select",
 		"type"=>"select",
 		"name"=>"Nombre",
 		"value"=>"Países",
@@ -79,12 +110,14 @@ $formularios["registro"] = [
 
 	//Radio
 	$inputs["radio1"] = [
+		"label"=>"Label del radio",
 		"type"=>"radio",
 		"name"=>"radio",
 		"value"=>"Radio1",
 	];
-	echo "<label>Radio2</label>";
+
 	$inputs["radio2"] = [
+		"label"=>"Label del radio",
 		"type"=>"radio",
 		"name"=>"radio",
 		"value"=>"Radio2",
@@ -93,12 +126,14 @@ $formularios["registro"] = [
 	//Checked
 
 	$inputs["check1"] = [
+		"label"=>"Label del checkbox",
 		"type"=>"checkbox",
 		"name"=>"check",
 		"value"=>"check1",
 	];
 
 	$inputs["check2"] = [
+		"label"=>"Label del checkbox",
 		"type"=>"checkbox",
 		"name"=>"check",
 		"value"=>"check2",
@@ -106,4 +141,7 @@ $formularios["registro"] = [
 
 $Formulario = new Formulario($formularios, $inputs);
 $Formulario->formulario();
+*/
+
+
 ?>
